2973166096529790cfe198e6d4882ea2a409a772
kimstudy0429 <kimstudy0429@gmail.com>
2026-06-13 19:17:06 +0900

    result 결과 출력 추가

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -2,8 +2,16 @@
 using namespace std;
 void main() {
 	const int arr1[3] = { 1, 2, 3 };
+	int a = arr1[0];		//OK: read-only access to arr1[0]
+	int result;
 
+	result = arr1[0] + arr1[1] + arr1[2];
+	
 	cout << arr1[0] << endl;
 	//arr1[0] = 5;			//Error: assignment of read-only location 'arr1[0]'
-	cout << arr1[0] << endl;
+	cout << arr1[0] << endl; 
+	cout << result << endl;
+
+
+	
 }
