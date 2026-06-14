1f56fe9d8ddb0e9252d7f227513adc79c85a5173
kimstudy0429 <kimstudy0429@gmail.com>
2026-06-14 18:06:50 +0900

    증감 연산자 추가

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+#if 0
 void main() {
 	const int arr1[3] = { 1, 2, 3 };
 	int a = arr1[0];		//OK: read-only access to arr1[0]
@@ -15,3 +16,23 @@ void main() {
 
 	
 }
+#endif
+//연산자
+#if 1
+void main() {
+	char a, b;
+	a = 10;		//대입 연산자
+	a++;		//후위형 증감 연산자
+	cout << +a << endl;	
+	++a;		//전위형 증감 연산자
+	cout << +a << endl;
+
+	b = 5;
+	b--;		//후위형 감소 연산자
+	cout << +b << endl;
+	--b;		//전위형 감소 연산자
+	cout << +b << endl;
+
+
+}
+#endif
